b86baae9c31ad44c55a40cb07a61621130f53f55
Ian Miell <ian.miell@gmail.com>
2017-01-22 21:05:14 +0000

    refactoring

--- shutit_standalone.py
+++ shutit_standalone.py
@@ -4,41 +4,22 @@ import shutit_util
 import shutit_setup
 
 
-# POC of shutit_standalone functionality.
-
-# TODO: tests, logging, error checking, consolidation of code that's been copied
+# TODO: logging, error checking
 def create_bash_session():
 	shutit = shutit_global.shutit
 	shutit_util.parse_args()
 	shutit_util.load_configs()
-	shutit_pexpect_session = shutit_pexpect.ShutItPexpectSession('target_child','/bin/bash')
-	target_child = shutit_pexpect_session.pexpect_child
-	shutit.set_default_shutit_pexpect_session(shutit_pexpect_session)                                                                                 
-	shutit.set_default_shutit_pexpect_session_expect(shutit.expect_prompts['base_prompt'])                                                            
-	prefix = 'ORIGIN_ENV'                                                                                                                             
-	shutit_pexpect_session.setup_prompt('origin_prompt', prefix=prefix)                                                                               
-	shutit_pexpect_session.login_stack_append(prefix)                                                                                                 
-	shutit_pexpect_session = shutit.get_shutit_pexpect_session_from_id('target_child')                                                               
-	shutit_pexpect_session.pexpect_child = target_child                                                                                               
-	shutit.set_default_shutit_pexpect_session_expect(shutit.expect_prompts['base_prompt'])                                                            
-	shutit.set_default_shutit_pexpect_session(shutit_pexpect_session)                                                                                 
-	shutit_pexpect_session.setup_prompt(prefix,prefix=prefix)                                                                                         
-	shutit_pexpect_session.login_stack_append(prefix)     
+	shutit_setup.setup_origin_environment(shutit)
 	return shutit
+
 
 def create_docker_session():
 	shutit = shutit_global.shutit
 	shutit_util.parse_args()
 	shutit_util.load_configs()
-	shutit_pexpect_session = shutit_pexpect.ShutItPexpectSession('target_child','/bin/bash')
-	target_child = shutit_pexpect_session.pexpect_child
+	shutit_setup.setup_origin_environment(shutit)
 
-	shutit.set_default_shutit_pexpect_session(shutit_pexpect_session)                                                                                 
-	shutit.set_default_shutit_pexpect_session_expect(shutit.expect_prompts['base_prompt'])                                                            
-	prefix = 'ORIGIN_ENV'                                                                                                                             
-	shutit_pexpect_session.setup_prompt('origin_prompt', prefix=prefix)                                                                               
-	shutit_pexpect_session.login_stack_append(prefix)                                                                                                 
-
+	prefix='root'
 	target_child = shutit_setup.conn_docker_start_container(shutit,'ORIGIN')
 	shutit_pexpect_session = shutit.get_shutit_pexpect_session_from_id('target_child')
 	shutit_pexpect_session.pexpect_child = target_child
@@ -49,5 +30,7 @@ def create_docker_session():
 	return shutit
 
 if __name__ == '__main__':
-	create_bash_session()
-	create_docker_session()
+	shutit = create_bash_session()
+	shutit.send('hostname')
+	shutit = create_docker_session()
+	shutit.send('hostname')
